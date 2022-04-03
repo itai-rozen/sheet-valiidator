0d974b0945803f4aed57be43840cb7c679e65924
itai rozen <itai.rozen@mail.huji.ac.il>
2022-04-03 16:17:00 +0300

    on file upload table is created in sql and datasheet is inserted to table

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -29,6 +29,17 @@ const Main = () => {
       getTableKeys(workSheet)
       const sheetData = XLSX.utils.sheet_to_json(workSheet)
       setSheetData(sheetData)
+      addRowsToTable(sheetData)
+    }
+  }
+
+  const addRowsToTable = async rows => {
+    for (const row of rows){
+      try {
+        await addRowToSql(row)
+      } catch(err){
+        console.log(err)
+      }
     }
   }
 
@@ -43,26 +54,23 @@ const Main = () => {
       if (cell && cell.t) headers.push(XLSX.utils.format_cell(cell))
     }
     setTableKeys(headers)
+    createSqlTable(headers)
   }
 
   const validateSheet = () => {
     setProblems([])
-    sheetData.forEach((row,i) => {
+    sheetData.forEach((row, i) => {
       console.log(row)
-      validateRow(row,i)
+      validateRow(row, i)
     })
   }
 
-  const validateRow = (rowObj,index) => {
+  const validateRow = (rowObj, index) => {
     for (const value in validations) {
-      // console.log('validations: ', validations)
-      // console.log('value: ', value)
-      // console.log('validations[value]: ', validations[value])
-      // console.log('row[value] :',rowObj[value])
       const currValidation = validations[value]
       const currValue = rowObj[value]
       let validationFunc
-      switch(currValidation){
+      switch (currValidation) {
         case 'email':
           validationFunc = validateEmail;
           break;
@@ -79,47 +87,78 @@ const Main = () => {
           validationFunc = console.log
           break;
       }
-      if (!validationFunc(currValue + '',index,value)) setProblems(prevArr => [...prevArr, {rowNum: rowObj.__rowNum__ , problem:currValidation, value:currValue}])
+      if (!validationFunc(currValue + '', index, value)) setProblems(prevArr => [...prevArr, { rowNum: rowObj.__rowNum__, problem: currValidation, value: currValue }])
     }
-  
-}
 
-const validateEmail = (str,_,_2) => str.match(/^\S+@\S+\.\S+$/)
+  }
 
-const validatePhone = (str,_,_2) => {
-  const cleanStr = cleanString(str)
-  return cleanStr.match(/^05[0-9]{8}$|^5[0-9]{8}$/)
-} 
+  const validateEmail = (str, _, _2) => str.match(/^\S+@\S+\.\S+$/)
 
-const validateFullCells = (str,_,_2) => str.trim() !== 'undefined'
+  const validatePhone = (str, _, _2) => {
+    const cleanStr = cleanString(str)
+    return cleanStr.match(/^05[0-9]{8}$|^5[0-9]{8}$/)
+  }
 
-const validateDuplicateCells = (str,i,col) => !sheetData.find((row,index) => (row[col] === str && i !== index))
+  const validateFullCells = (str, _, _2) => str.trim() !== 'undefined'
 
-const cleanString = str =>  str.replace(/^972|[+().]/g, '')
-  
-useEffect(() => {
-  console.log(cleanString('97266.21.56'))
-}, [])
+  const validateDuplicateCells = (str, i, col) => !sheetData.find((row, index) => (row[col] === str && i !== index))
 
-return <div className="main-container">
-  <h1>Sheet evaluator</h1>
-  <div className="input-container">
-    <input type="file" onChange={uploadFile} />
-    <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>add validations</button>
-    <button disabled={!tableKeys.length} onClick={validateSheet}>Validate</button>
+  const cleanString = str => str.replace(/^972|[+().]/g, '')
+
+  const addRowToSql = async (rowObj) => {
+    // const res = await (await fetch('http://localhost:3001/')).json()
+    // console.log(res)
+    console.log('json stringy : ', JSON.stringify(rowObj))
+    const rawResponse = await fetch('http://localhost:3001/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(rowObj)
+    });
+  }
+
+  const createSqlTable = async tableHeaders => {
+    try {
+      const rawResponse = await fetch('http://localhost:3001/create-table', {
+        method: 'POST',
+        body: JSON.stringify(tableHeaders),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+      console.log('after creating table: ', rawResponse)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  return <div className="main-container">
+    <h1>Sheet evaluator</h1>
+    <div className="input-container">
+      <input type="file" onChange={uploadFile} />
+      <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>add validations</button>
+      <button disabled={!tableKeys.length} onClick={validateSheet}>Validate</button>
+    </div>
+
+    <Validations validations={validations} />
+
+
+    <Problems problems={problems} />
+    {showModal && <Modal
+      setShowModal={setShowModal}
+      validations={validations}
+      setValidations={setValidations}
+      tableKeys={tableKeys}
+    />}
   </div>
-
-  <Validations  validations={validations} />
-
-
-  <Problems problems={problems} />
-  {showModal && <Modal
-    setShowModal={setShowModal}
-    validations={validations}
-    setValidations={setValidations}
-    tableKeys={tableKeys}
-  />}
-</div>
 }
 
 export default Main
