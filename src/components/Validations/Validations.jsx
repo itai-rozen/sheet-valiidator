e89c7827788335731b68737f4e3423f433ad6bdf
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-21 16:20:16 +0300

    headers are in hebrew

--- src/components/Validations/Validations.jsx
+++ src/components/Validations/Validations.jsx
@@ -4,9 +4,9 @@ import './validations.css'
 const Validations = ({ validations }) => {
   return <div className="validations-modal-container">
     <ul className="validation-list">
-      <li className='validation-headers item'><p>Field</p><p>Validation</p></li>
+      <li className='validation-headers item'><p>בדיקה</p><p>עמודה</p></li>
       {Object.keys(validations).map(valid => {
-        return <li className='validation-item item' key={valid+validations[valid]}><p>{valid}</p><p>{validations[valid]}</p></li>
+        return <li className='validation-item item' key={valid+validations[valid]}><p>{validations[valid]}</p><p>{valid}</p></li>
       })}
     </ul>
   </div>
