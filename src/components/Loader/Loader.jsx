c8e350329f5418dc98f9d503fcdbc29d2a7cfc90
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-21 21:15:37 +0300

    created loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return <div className="loader-container">
+    <div class="lds-circle">
+      <div></div>
+    </div>
+  </div>
+}
+
+export default Loader
