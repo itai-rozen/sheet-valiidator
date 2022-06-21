af6061b4427de2c9415ad90eaf738da454566ed4
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-22 01:15:33 +0300

    changed name of upload list button

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -7,7 +7,7 @@ const Inputs = ({ downloadFile,problems, uploadFile,tableKeys,setShowModal,valid
      <input className='file-input' type="file" onChange={uploadFile} />
       <button disabled={!tableKeys.length || !Object.keys(validations).length } onClick={validateSheet}>בדוק</button>
       <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>מפה את הקובץ</button>
-      <button disabled={(!tableKeys.length || problems.length > 0) || !validations.length}>טען רשימה</button>
+      <button disabled={(!tableKeys.length || problems.length > 0) || !validations.length}>טען שורות תקינות</button>
       {/* <button onClick={() => downloadFile()}>Download</button> */}
   </div>
 }
