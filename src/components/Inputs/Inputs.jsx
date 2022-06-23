469f52765009e5e542ddad1f71b30d85dd794e93
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-23 13:12:26 +0300

    added option of downloading valid data to xlsx file. removed validation check from component. it is available in Modal component

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import * as XLSX from 'xlsx'
 import './inputs.css'
 
-const Inputs = ({ downloadFile,problems, uploadFile,tableKeys,setShowModal,validateSheet, validations }) => {
+const Inputs = ({ 
+                  downloadFile,
+                  problems, 
+                  uploadFile,
+                  tableKeys,
+                  setShowModal,
+                  validData,
+                  validateSheet, 
+                  validations }) => {
   return <div className="input-container">
      <input className='file-input' type="file" onChange={uploadFile} />
-      <button disabled={!tableKeys.length || !Object.keys(validations).length } onClick={validateSheet}>בדוק</button>
+      {/* <button disabled={!tableKeys.length || !Object.keys(validations).length } onClick={validateSheet}>בדוק</button> */}
       <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>מפה את הקובץ</button>
-      <button disabled={(!tableKeys.length || problems.length > 0) || !validations.length}>טען שורות תקינות</button>
-      {/* <button onClick={() => downloadFile()}>Download</button> */}
+      <button disabled={!validData.length} onClick={() => console.log('valid data: ',validData)}>טען שורות תקינות</button>
+      <button disabled={!validData.length} onClick={() => downloadFile(validData, 'invites_valids')}>הורד שורות תקינות</button>
   </div>
 }
 
