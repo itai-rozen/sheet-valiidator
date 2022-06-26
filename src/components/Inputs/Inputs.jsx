d574d9fe4f239c321ec784a09bbbbd1ad471f7e2
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-26 13:49:11 +0300

    commented out validations component & button, removed unused props

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import * as XLSX from 'xlsx'
+
 import './inputs.css'
 
-const Inputs = ({ 
-                  downloadFile,
-                  problems, 
-                  uploadFile,
-                  tableKeys,
-                  setShowModal,
-                  validData,
-                  validateSheet, 
-                  validations }) => {
+const Inputs = ({
+  downloadFile,
+  // problems,
+  uploadFile,
+  // tableKeys,
+  // setShowModal,
+  sqlHeaders,
+  uploadToServer,
+  validData,
+  // validateSheet,
+  // validations 
+}) => {
+
+
   return <div className="input-container">
-     <input className='file-input' type="file" onChange={uploadFile} />
-      {/* <button disabled={!tableKeys.length || !Object.keys(validations).length } onClick={validateSheet}>בדוק</button> */}
-      <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>מפה את הקובץ</button>
-      <button disabled={!validData.length} onClick={() => console.log('valid data: ',validData)}>טען שורות תקינות</button>
-      <button disabled={!validData.length} onClick={() => downloadFile(validData, 'invites_valids')}>הורד שורות תקינות</button>
+    <input className='file-input' type="file" onChange={uploadFile} />
+    {/* <button disabled={!tableKeys.length || !Object.keys(validations).length } onClick={validateSheet}>בדוק</button> */}
+    {/* <button disabled={!tableKeys.length} onClick={() => setShowModal(true)}>מפה את הקובץ</button> */}
+    <button disabled={!validData.length || !sqlHeaders.target_name} onClick={uploadToServer}>טען שורות תקינות</button>
+    <button disabled={!validData.length} onClick={() => downloadFile(validData, 'invites_valids')}>הורד שורות תקינות</button>
   </div>
 }
 
