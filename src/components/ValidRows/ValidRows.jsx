9a54610140ee7f242126175acadf4563979a875f
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-28 13:03:56 +0300

    made changes in classes names

--- src/components/ValidRows/ValidRows.jsx
+++ src/components/ValidRows/ValidRows.jsx
@@ -23,15 +23,17 @@ const ValidRows = ({ validData, sqlHeaders, setSqlHeaders, tableKeys }) => {
         Object.keys(validData[0]).reverse()
         .filter(header => header !== 'rowNum')
         .map((header, i) =>
-          <li key={header + i} className='row-header'>
+          <li key={header + i} className='header-item'>
 
             {
               (getHeader(header)) ?
-                <p>{sqlHeaderMap[getHeader(header)]}
-                  {getHeader(header) === 'target_phone' ||
+                <p className='data-item'>{sqlHeaderMap[getHeader(header)]}
+                  {
+                  getHeader(header) === 'target_phone' ||
                     <span onClick={() => setSqlHeaders({ ...sqlHeaders, [getHeader(header)]: '' })}>❌</span>
                   }
                 </p> :
+                <p className='data-item'>
                 <select name="" id="" defaultValue='' onChange={e => setSqlHeaders({ ...sqlHeaders, [e.target.value]: header })} >
                   <>
                     <option disabled value=''>שייך שדה</option>
@@ -48,6 +50,7 @@ const ValidRows = ({ validData, sqlHeaders, setSqlHeaders, tableKeys }) => {
                     }
                   </>
                 </select>
+                </p>
             }
           </li>
         )
@@ -64,13 +67,7 @@ const ValidRows = ({ validData, sqlHeaders, setSqlHeaders, tableKeys }) => {
               .map((tableKey,i) => {
                return <p key={tableKey+i} className='data-item'>{validRow[tableKey]}</p> 
               })
-              // Object.values(validRow).reverse()
-              //   .map((validItem, j) => {
-              //     return (validItem) ?
-              //       <p key={validItem + j} className='data-item'>{validItem}</p> :
-              //       <p key={validItem + j} className='data-item'></p>
 
-              //   })
             }
           </li>
         })
