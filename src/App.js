35140768646888eb8476c2d839bcb7f075690a51
itai rozen <itai.rozen@mail.huji.ac.il>
2022-07-01 09:55:45 +0300

    removed import @index.js & made nesteed dynamic route

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
   return <div className="App">
     <Router>
       <Routes>
-        <Route element={<Main />}   path='/' />
-        <Route element={<Main />}   path='/:event_hash' />
+        <Route element={<Main />} path='/' >
+          <Route element={<Main />} path=':event_hash' />
+        </Route>
       </Routes>
     </Router>
   </div>
