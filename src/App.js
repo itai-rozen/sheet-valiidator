352ee9c0e38adad861b1041d72acb6b60265f414
itai rozen <itai.rozen@mail.huji.ac.il>
2022-07-03 00:22:45 +0300

    downgraded react-router-dom to 5.x

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Main from './components/Main/Main';
 
 function App() {
   return <div className="App">
     <Router>
-      <Routes>
-        <Route path="/" element={<Main />} >
-          <Route path=":event_hash" element={<Main />} />
-          <Route path="" element={<Main />} />
-        </Route>
-      </Routes>
+        <Route path="/:event_hash?" component={Main}  />
     </Router>
   </div>
 
