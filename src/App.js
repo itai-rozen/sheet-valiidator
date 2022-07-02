b5004e10f12c68ae3b70b31b32a775468d427d35
itai rozen <itai.rozen@mail.huji.ac.il>
2022-07-03 00:46:31 +0300

    added <Switch> to routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Main from './components/Main/Main';
 
 function App() {
   return <div className="App">
     <Router>
+      <Switch>
         <Route path="/:event_hash?" component={Main}  />
+      </Switch>
     </Router>
   </div>
 
