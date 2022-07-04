d8060e83b162dd8390de1dab09198ddef051806f
itai rozen <itai.rozen@mail.huji.ac.il>
2022-07-04 11:46:56 +0300

    class changed to className

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import './loader.css'
 
 const Loader = () => {
   return <div className="loader-container">
-    <div class="lds-circle">
+    <div className="lds-circle">
       <div></div>
     </div>
   </div>
